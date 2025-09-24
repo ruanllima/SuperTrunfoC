26f4d63349e1a08d28feb918cc0769bb7be6854b
ruanllima <ruanlimafn@gmail.com>
2025-09-24 19:53:03 -0300

    Super Trunfo Lógica | Nível Mestre | Adicionando a comparação entre dois atributos, somatória de atributos e implementação de dinamismo na seleção dos atributos com switch

--- superTrunfoLogica.c
+++ superTrunfoLogica.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main (){
 
@@ -19,7 +20,14 @@ int main (){
     float pibPerCapita2;
     float superPoder1;
     float superPoder2;
-    int opcaoAtributo;
+    char nomeAtributo1[40];
+    char nomeAtributo2[40];
+    int atributo1;
+    int atributo2;
+    int valoresAtributo1[2] = {0, 0};
+    int valoresAtributo2[2] = {0, 0};
+    int somaAtributos[2] = {0, 0};
+    int isDensidade1, isDensidade2 = 0;
 
     //calcula a densidade demografica das cartas
     densidadeDemografica1 = populacao1 / area1;
@@ -37,17 +45,147 @@ int main (){
 
     // menu interativo
     printf("\n\n======== Seja bem vindo ao Super Trunfo! ========\n");
-    printf("Escolha qual atributo deseja comparar:\n\n");
-    printf("1 - Populacao\n");
-    printf("2 - Area\n");
-    printf("3 - PIB\n");
-    printf("4 - PIB Per Capita\n");
-    printf("5 - Numero de Pontos Turisticos\n");
-    printf("6 - Densidade Demografica\n");
-    // solicita ao usuario o atributo desejado
-    scanf("%d", &opcaoAtributo);
+    printf("Escolha o primeiro atributo que deseja comparar:\n\n");
+    printf("1 - Populacao\n2 - Area\n3 - PIB\n4 - PIB Per Capita\n5 - Numero de Pontos Turisticos\n6 - Densidade Demografica\n");
+    scanf("%d", &atributo1);
+
+    switch(atributo1){
+    case 1:
+        printf("Escolha o segundo atributo que deseja comparar:\n\n");
+        printf("2 - Area\n3 - PIB\n4 - PIB Per Capita\n5 - Numero de Pontos Turisticos\n6 - Densidade Demografica\n");
+        scanf("%d", &atributo2);
+
+        strcpy(nomeAtributo1, "Populacao");
+        valoresAtributo1[0] = populacao1;
+        valoresAtributo1[1] = populacao2;
+    break;
+    case 2:
+        printf("Escolha o segundo atributo que deseja comparar:\n\n");
+        printf("1 - Populacao\n3 - PIB\n4 - PIB Per Capita\n5 - Numero de Pontos Turisticos\n6 - Densidade Demografica\n");
+        scanf("%d", &atributo2);
+
+        strcpy(nomeAtributo1, "Area");
+        valoresAtributo1[0] = area1;
+        valoresAtributo1[1] = area2;
+    break;
+    case 3:
+        printf("Escolha o segundo atributo que deseja comparar:\n\n");
+        printf("1 - Populacao\n2 - Area\n4 - PIB Per Capita\n5 - Numero de Pontos Turisticos\n6 - Densidade Demografica\n");
+        scanf("%d", &atributo2);
+
+        strcpy(nomeAtributo1, "Pib");
+        valoresAtributo1[0] = pib1;
+        valoresAtributo1[1] = pib2;
+    break;
+    case 4:
+        printf("Escolha o segundo atributo que deseja comparar:\n\n");
+        printf("1 - Populacao\n2 - Area\n3 - PIB\n5 - Numero de Pontos Turisticos\n6 - Densidade Demografica\n");
+        scanf("%d", &atributo2);
+
+        strcpy(nomeAtributo1, "Pib Per Capita");
+        valoresAtributo1[0] = pibPerCapita1;
+        valoresAtributo1[1] = pibPerCapita2;
+    break;
+    case 5:
+        printf("Escolha o segundo atributo que deseja comparar:\n\n");
+        printf("1 - Populacao\n2 - Area\n3 - PIB\n4 - PIB Per Capita\n6 - Densidade Demografica\n");
+        scanf("%d", &atributo2);
+
+        strcpy(nomeAtributo1, "Numero de Pontos Turisticos");
+        valoresAtributo1[0] = nPontosTuristicos1;
+        valoresAtributo1[1] = nPontosTuristicos2;
+    break;
+    case 6:
+        printf("\nEscolha o segundo atributo que deseja comparar:\n\n");
+        printf("1 - Populacao\n2 - Area\n3 - PIB\n4 - PIB Per Capita\n5 - Numero de Pontos Turisticos\n");
+        scanf("%d", &atributo2);
+
+        strcpy(nomeAtributo1, "Densidade Demografica");
+        valoresAtributo1[0] = densidadeDemografica1;
+        valoresAtributo1[1] = densidadeDemografica2;
+        isDensidade1 = 1;
+    break;
+    default:
+        printf("\nDigite um numero valido!\nExecute o programa novamente!");
+        return 1;
+    }
+
+    switch(atributo2){
+    case 1:
+        strcpy(nomeAtributo2, "Populacao");
+        valoresAtributo2[0] = populacao1;
+        valoresAtributo2[1] = populacao2;
+    break;
+    case 2:
+        strcpy(nomeAtributo2, "Area");
+        valoresAtributo2[0] = area1;
+        valoresAtributo2[1] = area2;
+    break;
+    case 3:
+        strcpy(nomeAtributo2, "Pib");
+        valoresAtributo2[0] = pib1;
+        valoresAtributo2[1] = pib2;
+    break;
+    case 4:
+        strcpy(nomeAtributo2, "Pib Per Capita");
+        valoresAtributo2[0] = pibPerCapita1;
+        valoresAtributo2[1] = pibPerCapita2;
+    break;
+        strcpy(nomeAtributo2, "Numero de Pontos Turisticos");
+        valoresAtributo2[0] = nPontosTuristicos1;
+        valoresAtributo2[1] = nPontosTuristicos2;
+    break;
+    case 6:
+        strcpy(nomeAtributo2, "Densidade Demografica");
+        valoresAtributo2[0] = densidadeDemografica1;
+        valoresAtributo2[1] = densidadeDemografica2;
+        isDensidade2 = 1;
+    break;
+    default:
+        printf("\nDigite um numero valido!\nExecute o programa novamente!");
+        return 1;
+    }
+
+    printf("\nCARTA 1 (%s) | CARTA 2 (%s)\n\n", nomePais1, nomePais2);
+    if ((valoresAtributo1[0] > valoresAtributo1[1]) && isDensidade1 == 0){
+        printf("ATRIBUTO 1 (%s):\n%s: %d (VENCEU)   |    %s: %d\n\n", nomeAtributo1, nomePais1, valoresAtributo1[0], nomePais2, valoresAtributo1[1]);
+    }
+    else{
+        printf("ATRIBUTO 1 (%s):\n%s: %d   |    %s: %d (VENCEU)\n\n", nomeAtributo1, nomePais1, valoresAtributo1[0], nomePais2, valoresAtributo1[1]);
+    }
+
+    if (valoresAtributo2[0] > valoresAtributo2[1] && isDensidade2 == 0){
+        printf("ATRIBUTO 2 (%s):\n%s: %d (VENCEU)   |    %s: %d\n\n", nomeAtributo2, nomePais1, valoresAtributo2[0], nomePais2, valoresAtributo2[1]);
+    }
+    else{
+        printf("ATRIBUTO 2 (%s):\n%s: %d   |    %s: %d (VENCEU)\n\n", nomeAtributo2, nomePais1, valoresAtributo2[0], nomePais2, valoresAtributo2[1]);
+    }
+
+    if (isDensidade1 == 1){
+        somaAtributos[0] = valoresAtributo2[0] - valoresAtributo1[0];
+        somaAtributos[1] = valoresAtributo2[1] - valoresAtributo1[1];
+    } else if (isDensidade2 == 1){
+        somaAtributos[0] = valoresAtributo1[0] - valoresAtributo2[0];
+        somaAtributos[1] = valoresAtributo1[1] - valoresAtributo2[1];
+    } else{
+        somaAtributos[0] = valoresAtributo1[0] + valoresAtributo2[0];
+        somaAtributos[1] = valoresAtributo1[1] + valoresAtributo2[1];
+    }
+
+    printf("SOMA (%s + %s)\n", nomeAtributo1, nomeAtributo2);
+    printf("(%s): %d   |   (%s): %d\n\n", nomePais1, somaAtributos[0], nomePais2, somaAtributos[1]);
+    if (somaAtributos[0] > somaAtributos[1]){
+        printf("%s VENCEU!", nomePais1);
+    } else if (somaAtributos[0] < somaAtributos[1]){
+        printf("%s VENCEU!", nomePais2);
+    }
+    else{
+        printf("EMPATE!");
+    }
 
 
+
+/*
     printf("\n\n======== COMPARACAO: ========\n");
     // utiliza o switch para comparar os valores de acordo com o atributo escolhido
     switch (opcaoAtributo){
@@ -100,7 +238,7 @@ int main (){
         } else if (pibPerCapita1 < pibPerCapita2){
             printf("CARTA 2 (%s) VENCEU!", nomePais2);
         } else if (pibPerCapita1 == pibPerCapita2){
-            printf("EMPATE!");
+            printf("EMPATE!"); 
         }
     break;
     case 5:
@@ -131,5 +269,5 @@ int main (){
     break;
 
     }
-
+*/
 }
